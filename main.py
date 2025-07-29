4c1fa966f1766270d37ab26d9f2d819374404688
Jan Welslau <jan.welslau@wibu.com>
2025-07-29 18:21:15 +0200

    git push wip main display

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 from app.timer import TimerController
+from app.display import Display
 import time
+from device.SH1106 import SH1106
+
 
 def main():
     timer = TimerController()
+    display = Display()
 
     while True:
         timer.update()
+        display.show_timer(timer)  # Update the display with timer info
         print(f"[Timer] Mode: {timer.mode}, Elapsed: {timer.elapsed:.2f}s")
         time.sleep(0.1)  # adjust for responsiveness vs CPU usage
 
